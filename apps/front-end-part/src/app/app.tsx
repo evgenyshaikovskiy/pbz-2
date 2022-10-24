7c18a257b835ad83c67675c6ab92b73efa763fd0
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-24 13:28:35 +0300

    setup projects

---
+++ apps/front-end-part/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import '../styles/app.css';
+
+export function App() {
+  return <div className="app">Main application</div>;
+}
+
+export default App;
