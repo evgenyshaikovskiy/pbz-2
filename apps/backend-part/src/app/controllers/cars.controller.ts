6c986367cc69e21ae6df50a28a243f810d70720a
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-27 00:38:38 +0300

    add cars endpoints

--- apps/backend-part/src/app/controllers/cars.controller.ts
+++ apps/backend-part/src/app/controllers/cars.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { useParams } from 'react-router-dom';
 import { AppService } from '../app.service';
 import { Car } from '../model/car.model';
 
 @Controller('cars')
 export class CarsController {
+  // TODO: proper answers for some requests
   constructor(private readonly appService: AppService) {}
   @Get()
   getAll() {
@@ -16,5 +18,22 @@ export class CarsController {
     const car: Car = request.body as unknown as Car;
     this.appService.addCar(car);
     return request;
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    this.appService.deleteFromRelationById(params.id, 'cars');
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    const res = this.appService.getByIdFromRelation(params.id, 'cars');
+    return res;
+  }
+
+  @Put(':id')
+  put(@Param() params, @Req() request: Request) {
+    const car: Car = request.body as unknown as Car;
+    this.appService.updateCarById(params.id, car);
   }
 }
