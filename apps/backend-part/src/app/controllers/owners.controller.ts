ef7c53112a688c3945586a235e656689d33882b9
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-27 00:45:43 +0300

    update owners controller

--- apps/backend-part/src/app/controllers/owners.controller.ts
+++ apps/backend-part/src/app/controllers/owners.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { AppService } from '../app.service';
 import { Owner } from '../model/owner.model';
 
@@ -7,6 +7,7 @@ export class OwnersController {
   constructor(private readonly appService: AppService) {}
   @Get()
   getAll(@Req() request: Request) {
+    // TODO: proper answers for some requests
     const res = this.appService.getAllOwners();
     // eslint-disable-next-line no-debugger
     console.log(request);
@@ -18,5 +19,21 @@ export class OwnersController {
     const owner: Owner = request.body as unknown as Owner;
     this.appService.addOwner(owner);
     return request;
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    this.appService.deleteFromRelationById(params.id, 'owners');
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    const res = this.appService.getByIdFromRelation(params.id, 'owners');
+  }
+
+  @Put(':id')
+  put(@Param() params, @Req() request: Request) {
+    const owner: Owner = request.body as unknown as Owner;
+    this.appService.updateOwnerById(params.id, owner);
   }
 }
