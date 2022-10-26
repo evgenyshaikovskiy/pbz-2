40dc25abbc0ff3361636d26423f49c21677507ca
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-27 00:42:13 +0300

    update inspection model and controller

--- apps/backend-part/src/app/controllers/inspections.controller.ts
+++ apps/backend-part/src/app/controllers/inspections.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { AppService } from '../app.service';
 import { Inspection } from '../model/inspection';
 
 @Controller('inspections')
 export class InspectionsController {
+  // TODO: proper answers for some requests
   constructor(private readonly appService: AppService) {}
   @Get()
   getAll() {
@@ -17,5 +18,22 @@ export class InspectionsController {
     const inspection: Inspection = request.body as unknown as Inspection;
     this.appService.addInspection(inspection);
     return request;
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    this.appService.deleteFromRelationById(params.id, 'inspections');
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    const res = this.appService.getByIdFromRelation(params.id, 'inspections');
+    return res;
+  }
+
+  @Put(':id')
+  put(@Param() params, @Req() request: Request) {
+    const inspection: Inspection = request.body as unknown as Inspection;
+    this.appService.updateInspectionById(params.id, inspection);
   }
 }
